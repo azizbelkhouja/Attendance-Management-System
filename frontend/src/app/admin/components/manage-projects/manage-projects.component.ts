55685c987e280e530a66d661ef543789b223b5b0
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-05-10 19:37:53 +0200

    add project form

---
+++ frontend/src/app/admin/components/manage-projects/manage-projects.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SharedModule } from '../../../shared/shared.module';
+
+@Component({
+  selector: 'app-manage-projects',
+  imports: [SharedModule],
+  templateUrl: './manage-projects.component.html',
+  styleUrl: './manage-projects.component.scss'
+})
+export class ManageProjectsComponent {
+
+  projectForm! : FormGroup;
+
+  constructor(fb:FormBuilder){}
+
+  ngOnInit() {
+    this.projectForm = this.fb.group({
+      name: [null, [Validators.required]],
+      duration: [null, [Validators.required]],
+      startDate: [null, [Validators.required]],
+    })
+  }
+}
