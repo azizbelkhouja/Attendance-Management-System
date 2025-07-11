47f7a0af515a0e706749ad25de654c71162a7f12
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-07-11 18:21:37 +0200

    Add employee management form and project selection
    Implemented a form for adding new employees with fields for name, email, password, and project selection. Integrated project fetching from the admin service and applied custom styles for the form and card components.

--- frontend/src/app/admin/components/manage-employees/manage-employees.component.ts
+++ frontend/src/app/admin/components/manage-employees/manage-employees.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AdminService } from '../../services/admin.service';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @Component({
   selector: 'app-manage-employees',
-  imports: [SharedModule],
+  imports: [SharedModule, NzSelectModule],
   templateUrl: './manage-employees.component.html',
   styleUrl: './manage-employees.component.scss'
 })
 export class ManageEmployeesComponent {
 
   employeeForm! : FormGroup;
+  projects: any;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private adminService: AdminService) {}
 
   ngOnInit(): void {
     this.employeeForm = this.fb.group({
@@ -20,6 +23,15 @@ export class ManageEmployeesComponent {
       email: [null, [Validators.required]],
       projectId: [null, [Validators.required]],
       password: [null, [Validators.required]]
+    });
+
+    this.getAllProjects();
+  }
+
+  getAllProjects() {
+    this.adminService.getProjects().subscribe(res=>{
+      this.projects = res;
+      console.log(this.projects);
     });
   }
 
