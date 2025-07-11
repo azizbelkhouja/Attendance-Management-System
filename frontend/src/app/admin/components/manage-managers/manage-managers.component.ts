ff6768223837e45dbeabe8b6c6e0f9e8dfd9315c
Mohamed Aziz Belkhouja <belkhoujamohamedaziz@gmail.com>
2025-07-11 15:33:37 +0200

    Add manager creation form and service integration
    Implemented a form in ManageManagersComponent to add new managers, including fields for name, email, password, and project selection. Integrated form validation and updated AdminService with an addUser method to handle manager creation via API.

--- frontend/src/app/admin/components/manage-managers/manage-managers.component.ts
+++ frontend/src/app/admin/components/manage-managers/manage-managers.component.ts
@@ -1,18 +1,36 @@
 import { Component } from '@angular/core';
 import { SharedModule } from '../../../shared/shared.module';
 import { AdminService } from '../../services/admin.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 
 @Component({
   selector: 'app-manage-managers',
-  imports: [SharedModule],
+  imports: [SharedModule, NzSelectModule],
   templateUrl: './manage-managers.component.html',
   styleUrl: './manage-managers.component.scss'
 })
 export class ManageManagersComponent {
 
   projects:any;
+  managerForm!: FormGroup;
 
-  constructor ( private adminService: AdminService) {}
+  constructor ( private adminService: AdminService,
+    private fb:FormBuilder,
+    private message: NzMessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.managerForm = this.fb.group({
+      name: [null, [Validators.required]],
+      email: [null, [Validators.required]],
+      projectId: [null, [Validators.required]],
+      password: [null, [Validators.required]]
+    });
+
+    this.getAllProjects();
+  }
 
   getAllProjects() {
 
